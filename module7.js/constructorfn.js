2e978ad1f5dd8ac19bdcc09691d08b65b764bd85
Satyam <tsingh30399@gmail.com>
2023-05-13 19:27:01 +0530

    Constructor

---
+++ module7.js/constructorfn.js
@@ -0,0 +1,10 @@
+function createCar(_name, _company, _color){
+    this.name = _name
+    this.company = _company
+    this.color = _color
+}
+
+
+let car1 = new createCar('X4', 'BMW', 'Red')
+
+console.log(car1)
