b9ad05f57be2fe3d8ff7d8d6401ec14bbf0045e1
Satyam <tsingh30399@gmail.com>
2023-05-15 19:57:02 +0530

    Exercise

---
+++ module7.js/que2.js
@@ -0,0 +1,9 @@
+function Person(firstName, lastName){
+    this.firstName = firstName
+   
+    this.lastName = lastName
+}
+const lydia = new Person('Satyam', 'Singh')
+const serah = Person('Atif', 'Hu')
+console.log(lydia)
+console.log(serah)
