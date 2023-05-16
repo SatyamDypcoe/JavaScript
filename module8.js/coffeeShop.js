f3fb46a29f8d88266490e44da1b235d566994c24
Satyam <tsingh30399@gmail.com>
2023-05-17 02:14:29 +0530

    promises coffee shop example

---
+++ module8.js/coffeeShop.js
@@ -0,0 +1,26 @@
+function placeOrder(drink){
+    return new Promise(function(resolve, reject){
+        if(drink==='coffee'){
+            resolve('Order for coffee recieved')
+        }
+        else{
+            reject('Other order rejected')
+        }
+    })
+}
+
+function processOrder(order){
+    return new Promise(function(resolve){
+        console.log('Order is being processed')
+        resolve(`${order} is served`)
+    })
+}
+
+
+placeOrder('coffee').then(function(orderPlaced){
+    console.log(orderPlaced)
+    let orderIsProcessed = processOrder(orderPlaced)
+    return orderIsProcessed
+}).then(function(processedOrder){
+    console.log(processedOrder)
+})
