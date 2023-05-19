e59aecab0023915e34966cd2dff9fdaa52ea1091
Satyam <tsingh30399@gmail.com>
2023-05-19 21:24:47 +0530

    truthy and falsy

---
+++ module9.js/truthyFalsy.js
@@ -0,0 +1,25 @@
+let a = null
+
+if(a){
+
+    console.log('I am truthy')
+
+}
+
+else{
+
+    console.log('I am falsy')
+
+}
+
+// * False
+
+// 0 (zero)
+// -0 (minus zero)
+// 0n (BigINt zero)
+// '', "", `` (empty string)
+// null
+// undefined
+// NaN
+
+// * Let every other values return True.
