d148c30168cae9556606a0a4dc4d4fe335498d0c
Satyam <97747462+SatyamDypcoe@users.noreply.github.com>
2023-05-19 21:02:00 +0530

    Delete comparision.js

--- comparision.js
+++
@@ -1,8 +0,0 @@
-let a = 2
-
-let b ='2' 
-
-console.log(a==b) // (lose checking) double equal check only value not data types.
-
-
-console.log(a===b)  // (strict checking) tripple equal check both value and data types.
