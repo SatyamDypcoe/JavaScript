8615eb8ce5b5f8dbfddfc2c3acd55662a7f82ee7
Satyam <tsingh30399@gmail.com>
2023-05-20 23:37:30 +0530

    Shallow copy

--- module9.js/ShallowCopy.js
+++ module9.js/ShallowCopy.js
@@ -3,10 +3,13 @@
 let firstPerson ={
     name: 'Satyam',
     age : 24
+
+    
 }
 
 let secondPerson = {...firstPerson}
 secondPerson.name = 'Atif'
+
  
 console.log(firstPerson)
 console.log(secondPerson)
