378c1cfff0608cbb8f7925c1a3234ba25996dd2e
Satyam <tsingh30399@gmail.com>
2023-05-23 20:43:41 +0530

    Null, Undefined, not defined

---
+++ module9.js/nullUndefinedNotDefined.js
@@ -0,0 +1,27 @@
+// undefined
+
+let a; 
+
+console.log(a)
+
+let arr = [1, 2, , 4]
+
+console.log(arr[2])
+
+function add(a, b){
+        let c = a+b
+        //return c
+}
+
+let sum = add(2, 3)
+console.log(sum)
+
+// Null
+
+let myLife = null
+
+console.log(myLife)
+
+// not defined
+
+console.log(b)
