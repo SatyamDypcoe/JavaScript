142942d3076cb8f4383cc26c5cac82ad7d1b5908
Satyam <tsingh30399@gmail.com>
2023-05-23 19:54:33 +0530

    closures nested

--- module9.js/closrsNestedFn.js
+++ module9.js/closrsNestedFn.js
@@ -4,9 +4,24 @@ function greet(){
     function displayName(){
         console.log('hi ' + name)
 
+        let age = 25
+
+        function displayAge(){
+            console.log('My age is '+age)
+        }
+
+        return displayAge
+
     }
     return displayName
 }
 
 let g1 = greet()
-g1()
+
+// g1()  //displayName
+
+let g2 = g1()
+
+console.log(g2)
+
+g2()  // displayAge
