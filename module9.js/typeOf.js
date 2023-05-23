a79126a92566b42db6b47e15b8803ff9647a20dd
Satyam <tsingh30399@gmail.com>
2023-05-23 20:52:52 +0530

    Type of Operator

---
+++ module9.js/typeOf.js
@@ -0,0 +1,24 @@
+let a  = 2
+
+
+console.log(typeof a)
+
+let b = 'satyam'
+
+console.log(typeof b)
+
+let c = true
+
+console.log(typeof c)
+
+let myObj = {
+    name: 'satyam',
+    age: 24
+}
+
+console.log(typeof myObj)
+
+
+let arr = [1, 2, 3]
+
+console.log(typeof arr + 'Array')
